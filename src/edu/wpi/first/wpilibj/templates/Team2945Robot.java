5ff21e320c686b27a6d4b57422f27dac0d05f1fc
Bryce Guinta <azrathud@gmail.com>
2013-01-20 03:10:46 -0700

    add comment to test mode; move around putData functions

--- src/edu/wpi/first/wpilibj/templates/Team2945Robot.java
+++ src/edu/wpi/first/wpilibj/templates/Team2945Robot.java
@@ -136,13 +136,13 @@ public class Team2945Robot extends IterativeRobot {
      */
     public void testIint() {
         getWatchdog().setEnabled(false);
-    }
-    public void testPeriodic() {
-        getWatchdog().feed();
-        LiveWindow.run();
         SmartDashboard.putData(new AcquisitionForward());
         SmartDashboard.putData(new AcquisitionReverse());
         SmartDashboard.putData(new AcquisitionStop());
+    }
+    public void testPeriodic() {
+        getWatchdog().feed(); // running feed anyway.. 
+        LiveWindow.run();
 
     }
 }
