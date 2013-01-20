32fa12aabca14316598138a2057a0df847dfdfd6
Bryce Guinta <azrathud@gmail.com>
2013-01-20 03:20:53 -0700

    refactor

---
+++ src/edu/wpi/first/wpilibj/templates/commands/ShootingOn.java
@@ -0,0 +1,34 @@
+package edu.wpi.first.wpilibj.templates.commands;
+
+import edu.wpi.first.wpilibj.Relay;
+
+public class ShootingOn extends CommandBase {
+    
+    public shootingOn() {
+        // Use requires() here to declare subsystem dependencies
+        requires(shooting);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+        shooting.setshootingMotors(Relay.Value.kForward);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
