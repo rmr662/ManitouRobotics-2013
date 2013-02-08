5a3bcac1b5e98c1e4af2a4487c7ae62fb02e25fc
Bryce Guinta <azrathud@gmail.com>
2013-02-07 18:03:55 -0700

    playback should read; logger should write. I've changed the fileconnections to reflect this.

--- src/com/github/manitourobotics/robot/Logger.java
+++ src/com/github/manitourobotics/robot/Logger.java
@@ -68,7 +68,7 @@ public class Logger {
             return;
         }
         try {
-            fileInputConnection = (FileConnection) Connector.open("file://" + filename + ".txt", Connector.WRITE);
+            fileInputConnection = (FileConnection) Connector.open("file://" + filename + ".txt", Connector.READ);
             in = fileInputConnection.openDataInputStream();
         } catch (IOException e) {
             System.out.println("cannot read file");
@@ -174,7 +174,7 @@ public class Logger {
                 // Find a unique filename log<x>.txt where x is a number. Never erase a logCheck
                 // One must move the logCheck to final.txt (Get a ftp client under /ni-rt/system/) 
                 // to actually read the log
-                fileOutputConnection = (FileConnection) Connector.open("file://log" + Integer.toString(fileNumber) + ".txt", Connector.READ);
+                fileOutputConnection = (FileConnection) Connector.open("file://log" + Integer.toString(fileNumber) + ".txt", Connector.WRITE);
 
                 fileNumber += 1;
             } while (!fileOutputConnection.exists());
