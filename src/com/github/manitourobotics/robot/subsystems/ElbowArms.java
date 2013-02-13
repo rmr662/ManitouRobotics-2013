bb20e54e2647fe6eaf952b71057464ade58a6a79
Bryce Guinta <azrathud@gmail.com>
2013-02-13 14:26:45 -0700

    add logic for encoders for elbow arms; I need to test and to enable them in the constructor

--- src/com/github/manitourobotics/robot/subsystems/ElbowArms.java
+++ src/com/github/manitourobotics/robot/subsystems/ElbowArms.java
@@ -5,6 +5,7 @@
 package com.github.manitourobotics.robot.subsystems;
 
 import com.github.manitourobotics.robot.RobotMap;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -20,13 +21,50 @@ public class ElbowArms extends Subsystem {
     private static final double Ki = 0.0;
     private static final double Kd = 0.0;
 
-    private static final int MOTOR_STOP = 0;
+    private final int MOTOR_STOP = 0;
     
     Jaguar elbowArmsMotor = new Jaguar(RobotMap.PWM_ELBOW_ARMS);
 
+    Encoder encoder = new Encoder(RobotMap.IO_ELBOW_ENCODER_A, RobotMap.IO_ELBOW_ENCODER_B);
+    private final double DEFAULT_ANGLE = 45; // default angle of the arm relative to the ground
+    private final double MIN_ANGLE = 45;
+    private final double MAX_ANGLE = 95;
+    private final double ANGLE_CHANGE_PER_REVOLUTION = 2;
+    // to be tested
+    private final double MOVEMENT_PER_REVOLUTION = 2;
+
+    private boolean encoderEnabled = false;
+    double currentAngle = DEFAULT_ANGLE;
+
+
+    private double getTotalAngleChange () { //change from default angle
+        double revolutions = encoder.get()/1024;
+        double angleDifference = revolutions * 2;
+        return angleDifference;
+
+    }
+    public void ElbowArms(boolean encoderEnabled) {
+        this.encoderEnabled = encoderEnabled;
+        encoder.start();
+    }
+    public void ElbowArms() {
+    }
+
     public void setElbowArmSpeed(double speed) {
+        if(encoderEnabled) {
+            currentAngle = DEFAULT_ANGLE + getTotalAngleChange();
+
+            if(currentAngle <= MIN_ANGLE && speed < 0) { // I'm assuming negative speed means smaller angle
+                elbowArmsMotor.set(0);
+                return;
+            } else if(currentAngle >= MAX_ANGLE && speed > 0) { // I'm assume positive speed means greater angle
+                elbowArmsMotor.set(0);
+                return;
+            }
+        }
         elbowArmsMotor.set(speed);
     }
+
     // Initialize your subsystem here
     public ElbowArms() {
         //super("ElbowArms", Kp, Ki, Kd);
