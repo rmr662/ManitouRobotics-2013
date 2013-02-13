8d06ab60e8bb0162b258a02643e7dec65bba4662
Bryce Guinta <azrathud@gmail.com>
2013-02-13 09:05:52 -0700

    add prototype class for exctracting information from raspberry pi packets

---
+++ src/com/github/manitourobotics/robot/network/SocketReader.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.manitourobotics.robot.network;
+
+import com.sun.squawk.util.StringTokenizer;
+
+/**
+ *
+ * @author robotics
+ */
+public class SocketReader {
+    // takes information from the raspberry pi and then disects them for other command inspection
+    StringTokenizer tok;
+    String latestData;
+
+    static final int DISTANCE_FROM_TARGET = 1;
+
+    double targetDistance;
+
+    void giveData(String data) {
+        latestData = data;
+        int commandType;
+
+        tok = new StringTokenizer(latestData, ":");
+        commandType = Integer.parseInt(tok.nextToken());
+
+        if(commandType == DISTANCE_FROM_TARGET) {
+            targetDistance = Double.parseDouble(tok.nextToken());
+        }
+        else {
+            System.out.println("data not found");
+        }
+
+    }
+
+    double getTargetDistance() {
+        return targetDistance;
+    }
+    
+}
