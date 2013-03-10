a2b79a2297435e559f0f5bdafc6065e40f568996
Bryce Guinta <azrathud@gmail.com>
2013-03-10 12:53:40 -0600

    Revert "Revert "Change the shooter motors from going backward to going forward on the ShootingOn command. Hopefully the BaneBot is wired correctly on the final robot, or this commit will need to be reverted, and the wires on the practice robot will need to be reversed.""
    This reverts commit b141ef64a026c5a196e2958ee6a98515a4ee0d55.

--- src/com/github/manitourobotics/robot/commands/ShootingOn.java
+++ src/com/github/manitourobotics/robot/commands/ShootingOn.java
@@ -12,7 +12,7 @@ public class ShootingOn extends CommandBase {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-        shooting.setShootingMotors(Relay.Value.kForward);
+        shooting.setShootingMotors(Relay.Value.kReverse);
     }
 
     // Called repeatedly when this Command is scheduled to run
