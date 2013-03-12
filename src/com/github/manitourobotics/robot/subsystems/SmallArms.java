2203b4447b86101b4e1a2b7a0ce9ac6dd288c914
Bryce Guinta <azrathud@gmail.com>
2013-03-12 15:33:05 -0600

    testing out small arms code

--- src/com/github/manitourobotics/robot/subsystems/SmallArms.java
+++ src/com/github/manitourobotics/robot/subsystems/SmallArms.java
@@ -12,39 +12,86 @@ public class SmallArms extends Subsystem {
     private double moveSpeed = 1;
 
 
-    private final double MIN_ANGLE = 0;
-    private final double MAX_ANGLE = 90;
-    private final double OFFSET = 230; // voltage to angle is off by a constant
+    private final double ABS_MIN_ANGLE = 0;
+    private final double ABS_MAX_ANGLE = 90;
 
     private boolean encoderEnabled = false;
     private double baseSpeed = 0;
+    private double baseVoltage = 0;
+    private double baseAngle = 0;
+    private double minRelAngle = 0;
+    private double maxRelAngle = 0;
 
     // voltage yet to be turned into degrees
     AnalogChannel absAngle = new AnalogChannel(RobotMap.ANALOG_T_REX_ENCODER);
 
     public SmallArms(boolean encoderEnabled) {
         this.encoderEnabled = encoderEnabled;
+        baseVoltage = absAngle.getVoltage();
+        baseAngle = baseVoltage * 360/4.9 * -1 + 360 + 90;
+         maxRelAngle = baseAngle;
+         minRelAngle = baseAngle - 90;
+         if(minRelAngle < 0) {
+             minRelAngle = 360 - minRelAngle;
+         }
+
+    }
+
+    private boolean isInRange(double minRelAngle, double maxRelAngle, double testAngle) {
+        if( minRelAngle > maxRelAngle) {
+            if(minRelAngle <= testAngle && testAngle <= 360 || testAngle <= maxRelAngle && testAngle >= 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+            if( maxRelAngle > minRelAngle) {
+                if(testAngle <= maxRelAngle && testAngle >= minRelAngle) {
+                return true; 
+            }
+            return false;
+        }
+
+        return false;
+        
+    }
+
+    private double getAbsAngle(double minRelAngle, double maxRelAngle, double testAngle) {
+        if(minRelAngle > maxRelAngle && testAngle <= 360) {
+            if(testAngle <= maxRelAngle) {
+                return 90 - (maxRelAngle - testAngle);
+            } else {
+            return testAngle - minRelAngle;
+            }
+        }
+        else {
+            return maxRelAngle - testAngle;
+        }
+
     }
     public SmallArms() {
     }
 
 
+
     public double getAngle() {
-        double angle = absAngle.getVoltage() * -1 * 360/4.9 + OFFSET ;  // -1 is because the voltage increases as the angle decreases
+        double relAngle = absAngle.getVoltage() * -1 * 360/4.9 + 360 ;  // -1 is because the voltage increases as the angle decreases
+        double angle = getAbsAngle(minRelAngle, maxRelAngle, relAngle);
         SmartDashboard.putNumber("small arms voltage", absAngle.getVoltage());
         SmartDashboard.putNumber("small arms Angle", angle);
         return angle;
     }
 
     public double getOverComeGravity(double currentAngle) {
-        return .00007407*currentAngle*currentAngle-.00111111*currentAngle-.5;
+        return -0.000074074074074*currentAngle*currentAngle-.00111111111111*currentAngle+.5;
 
     }
 
     public void moveSmallArmsUp() {
         if(encoderEnabled) {
             double angle = getAngle();
-            if(angle > MAX_ANGLE) {
+            if(angle > ABS_MAX_ANGLE) {
                 stopSmallArms();
                 return;
             }
@@ -55,7 +102,7 @@ public class SmallArms extends Subsystem {
     public void moveSmallArmsDown() {
         if(encoderEnabled) {
             double angle = getAngle();
-            if(angle < MIN_ANGLE) {
+            if(angle < ABS_MIN_ANGLE) {
                 stopSmallArms();
                 return;
             }
@@ -67,6 +114,7 @@ public class SmallArms extends Subsystem {
     public void stopSmallArms() {
         if(encoderEnabled) {
             baseSpeed = getOverComeGravity(getAngle());
+            baseSpeed = 0;
         }
         smallArmsMotor.set(baseSpeed);
     }
